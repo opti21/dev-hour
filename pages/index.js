992710d5620acef8504d051626413615f86a9a61
opti21 <40129778+opti21@users.noreply.github.com>
2022-02-16 19:53:54 -0600

    Added tracking and changed to just gif

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import redirect from "nextjs-redirect"
-
-const Redirect = redirect("https://youtu.be/4v8ek9TEeOU?t=41")
+import redirect from "nextjs-redirect";
+import { useEffect } from "react";
+import splitbee from "@splitbee/web";
 
 export default function Home() {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+
+    splitbee.track()
+  }, []);
   return (
-    <Redirect>
-    <div className={styles.container}>
-      <Head>
-        <title>Dev Hour Podcast - Home</title>
-        <meta content="Dev Hour Podcast - Home" property="og:title" />
+      <div className={styles.container}>
+        <Head>
+          <title>Dev Hour Podcast - Home</title>
+          <meta content="Dev Hour Podcast - Home" property="og:title" />
 
-        <meta content="Dev Hour Podcast" property="og:site_name" />
+          <meta content="Dev Hour Podcast" property="og:site_name" />
 
-        <meta
-          content="https://cdn.discordapp.com/attachments/806262681920602143/937883694788657222/Dev_hour.jpg"
-          property="og:image"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+          <meta
+            content="https://cdn.discordapp.com/attachments/806262681920602143/937883694788657222/Dev_hour.jpg"
+            property="og:image"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
 
-      <div>
-        <h1>Dev Hour Podcast</h1>
+        <div>
+          <h1>Dev Hour Podcast</h1>
+          <h1>DEVHOUR</h1>
+          <Image src={"https://c.tenor.com/E3BS4gXTT6AAAAAC/deez-nuts.gif"} alt="DEEZ NUTS" width={500} height={500}/>
+    
+        </div>
       </div>
-    </div>
-    </Redirect>
   );
 }
