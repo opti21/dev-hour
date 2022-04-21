6a796be25a7ecf16a04a4a4235fa4e565abbfccc
opti21 <40129778+opti21@users.noreply.github.com>
2022-04-20 21:35:40 -0500

    this is the best frontend in the world

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import redirect from "nextjs-redirect";
 import { useEffect } from "react";
 import splitbee from "@splitbee/web";
+import ReactPlayer from "react-player/youtube";
 
 export default function Home() {
   useEffect(() => {
@@ -12,29 +13,34 @@ export default function Home() {
       apiUrl: "/_hive",
     });
 
-    splitbee.track()
+    splitbee.track();
   }, []);
   return (
-      <div className={styles.container}>
-        <Head>
-          <title>Dev Hour Podcast - Home</title>
-          <meta content="Dev Hour Podcast - Home" property="og:title" />
+    <div className={styles.container}>
+      <Head>
+        <title>Dev Hour Podcast - Home</title>
+        <meta content="Dev Hour Podcast - Home" property="og:title" />
 
-          <meta content="Dev Hour Podcast" property="og:site_name" />
+        <meta content="Dev Hour Podcast" property="og:site_name" />
 
-          <meta
-            content="https://cdn.discordapp.com/attachments/806262681920602143/937883694788657222/Dev_hour.jpg"
-            property="og:image"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
+        <meta
+          content="https://cdn.discordapp.com/attachments/806262681920602143/937883694788657222/Dev_hour.jpg"
+          property="og:image"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
 
-        <div>
-          <h1>Dev Hour Podcast</h1>
-          <h1>DEVHOUR</h1>
-          <Image src={"https://c.tenor.com/E3BS4gXTT6AAAAAC/deez-nuts.gif"} alt="DEEZ NUTS" width={500} height={500}/>
-    
-        </div>
+      <div>
+        <h1>Dev Hour Podcast</h1>
+        <h1>DEVHOUR</h1>
+        <ReactPlayer url="https://www.youtube.com/watch?v=7gEbHsHXdn0" />
+        <Image
+          src={"https://c.tenor.com/E3BS4gXTT6AAAAAC/deez-nuts.gif"}
+          alt="DEEZ NUTS"
+          width={300}
+          height={300}
+        />
       </div>
+    </div>
   );
 }
